76e53ecc0cc99c269c0fcd8040f3b180a2d1f776
Anthony Bloomer <ant0@protonmail.ch>
2017-03-04 13:55:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ setup(
     entry_points={
         "console_scripts": ['rcp = rcp.rcp:main']
     },
-    version='0.0.2',
+    version='0.0.3',
     description="Simple script to scrape polling data from RealClearPolitics and output as .csv",
     long_description=long_descr,
+    keywords=['politics', 'polls', 'realclearpolitics', 'web scraping'],
     author="Anthony Bloomer",
     author_email="ant0@protonmail.ch",
     url="https://github.com/AnthonyBloomer/rcp",
@@ -21,4 +22,8 @@ setup(
         'beautifulsoup4',
         'requests'
     ],
+    classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7'
+    ]
 )
