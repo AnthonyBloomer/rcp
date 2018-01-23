164c278214647624ddda9a0099107f1b6f36c354
Anthony Bloomer <ant0@protonmail.ch>
2018-01-23 22:31:00 +0000

    Download multiple polls.

--- rcp/rcp.py
+++ rcp/rcp.py
@@ -7,27 +7,29 @@ try:
 except ImportError:
     from urllib2 import urlopen
 
+parser = argparse.ArgumentParser()
+parser.add_argument("url", nargs='+', help="The url of the polling data.")
+parser.add_argument("--output", nargs="?", help="The output file name.")
+args = parser.parse_args()
+
 def main():
-    response = urlopen(args.url)
-
-    soup = BeautifulSoup(response, 'html.parser')
-
-    fp = soup.find("div", {"id": 'polling-data-full'})
-    rows = fp.find('table', {"class": 'data'})
-
-    p = []
-    for row in rows:
-        cols = row.find_all(['th', 'td'])
-        p.append([ele.text.strip() for ele in cols])
-
-    with open(args.output, "w") as f:
-        writer = csv.writer(f)
-        writer.writerows(p)
+    for pd in args.url:
+        response = urlopen(pd)
+        soup = BeautifulSoup(response, 'html.parser')
+        fp = soup.find("div", {"id": 'polling-data-full'})
+        rows = fp.find('table', {"class": 'data'})
+        p = []
+        for row in rows:
+            cols = row.find_all(['th', 'td'])
+            p.append([ele.text.strip() for ele in cols])
+        
+        fn = pd.rsplit('/', 1)[-1]
+        fn = fn[:-5] + ".csv"
+        print(fn)
+        with open(fn, "w") as f:
+            writer = csv.writer(f)
+            writer.writerows(p)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url", help="The url of the polling data.")
-    parser.add_argument('-o', "--output", nargs="?", help="The output file name. Defaults to output.csv", default="output.csv")
-    args = parser.parse_args()
     main()
