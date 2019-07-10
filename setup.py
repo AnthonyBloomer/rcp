7fd57c4a37b5da6cccf863d92395b6e8d0e71b8f
Anthony Bloomer <ant0@protonmail.ch>
2019-07-10 11:13:50 +0100

    Pushed 1.0.0 to git

--- setup.py
+++ setup.py
@@ -1,9 +1,45 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
 
 with open("README.rst", "rb") as f:
     long_descr = f.read().decode("utf-8")
+    
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
 
 setup(
     name="realclearpolitics",
@@ -11,7 +47,7 @@ setup(
     entry_points={
         "console_scripts": ['rcp = rcp.rcp:main']
     },
-    version='0.0.3',
+    version='1.0.0',
     description="Simple script to scrape polling data from RealClearPolitics and output as .csv",
     long_description=long_descr,
     keywords=['politics', 'polls', 'realclearpolitics', 'web scraping'],
@@ -25,5 +61,8 @@ setup(
     classifiers=[
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 2.7'
-    ]
+    ],
+    cmdclass={
+        'publish': PublishCommand,
+    },
 )
